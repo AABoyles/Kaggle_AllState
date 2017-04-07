f2fc36582ea1896b28d599ceecdde59eb93d3e4a
Tony Boyles <Anthony@Boyles.cc>
2017-04-07 11:34:39 -0400

    Cleanup commit

--- predict.py
+++ predict.py
@@ -3,36 +3,50 @@
 import numpy as np
 import pandas as pd
 import xgboost as xgb
+from sklearn.metrics import mean_absolute_error
 import feather
+
+# if you're paranoid about overfitting, increase this.
+n_folds = 10
+
+# if you see metrics dropping precipitously until the end, increase this.
+n_rounds = 100
+
+# Set this to anything you want.
+seed = 10
 
 # Load Training Data
 train = feather.read_dataframe("temp/preparedtraining.feather")
 train_labels = np.array(train['log_loss'])
-train_ids = train['id'].values.astype(np.int32)
 train.drop(train.columns[[-1,-2]], 1, inplace = True)
 train_d = xgb.DMatrix(train, label=train_labels)
 
 # Load Test Data
 test = feather.read_dataframe("temp/preparedtest.feather")
-test_ids = test['id'].values.astype(np.int32)
 test_d = xgb.DMatrix(test)
-
-n_rounds = 100
 
 # Discovered by the hyperoptimize.py script
 params = {
-    "colsample_bytree": .4928,
-    "eta": .1402,
-    "gamma": 2.6277,
-    "max_depth": 12,
-    "min_child_weight": 1.05,
-    "subsample": .8433
+    "colsample_bytree": .9921,
+    "eta": .0995,
+    "gamma": 3.8581,
+    "max_depth": 11,
+    "min_child_weight": 1.0065,
+    "subsample": 1
 }
 
-model = xgb.train(params, train_d, n_rounds, [(train_d, 'train')], verbose_eval = False,)
+def xg_eval_mae(yhat, dtrain):
+    y = dtrain.get_label()
+    return 'mae', mean_absolute_error(np.exp(y), np.exp(yhat))
+
+res = xgb.cv(params, train_d, n_rounds, n_folds, early_stopping_rounds = 15, seed = seed, feval = xg_eval_mae)
+
+n_rounds = res.shape[0] - 1
+
+model = xgb.train(params, train_d, n_rounds)
 
 # Write the Results
 result = pd.DataFrame(np.exp(model.predict(test_d)), columns=['loss'])
-result["id"] = test_ids
+result["id"] = test['id'].values.astype(np.int32)
 result = result.set_index("id")
 result.to_csv('outputs/hyperoptimizedxgb.csv', index=True, index_label='id')
--- prep.py
+++ prep.py
@@ -3,7 +3,7 @@
 import numpy as np
 import pandas as pd
 from scipy.stats import skew, boxcox
-from sklearn.preprocessing import StandardScaler
+import statsmodels.formula.api as smf
 import feather
 
 # Load Training Data
@@ -12,6 +12,15 @@ train['log_loss'] = np.log(train['loss'])
 
 # Load Test Data
 test = pd.read_csv('data/test.csv', dtype={'id': np.int32})
+
+def mean_corr(log=False):
+    var = 'loss'
+    if log: var = 'log_loss'
+    mean_correlation = 0
+    for feat in test.dtypes[test.dtypes != "object"].index:
+        mean_correlation = mean_correlation + (np.corrcoef(train[feat], train[var])**2)[0,1]
+    mean_correlation = mean_correlation/len(features_numeric)
+    return(mean_correlation)
 
 # compute skew and do Box-Cox transformation
 # transform features with skew > 0.25 (this can be varied to find optimal value)
@@ -30,6 +39,14 @@ for feat in features_categorical:
     a[feat] = a.index
     train[feat] = pd.merge(left=train, right=a, how='left', on=feat)['log_loss_y']
     test[feat] = pd.merge(left=test, right=a, how='left', on=feat)['log_loss']
+
+temp = test.dropna(1)
+model = smf.ols('log_loss ~ ' + ' + '.join(temp.columns), data=train.drop('loss', 1)).fit()
+yhat = np.exp(model.predict(test))
+
+result = pd.DataFrame({'id': test['id'].values, 'loss': yhat})
+result = result.set_index('id')
+result.to_csv('outputs/simplelm.csv', index=True, index_label='id')
 
 # Save the dataframes in files to be picked up by other scripts
 feather.write_dataframe(train, "temp/preparedtraining.feather")
